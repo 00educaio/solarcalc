b479fd1b5619db30f04bfd848790ab48ec2974a6
Caio Eduardo <gostharker@gmail.com>
2025-06-16 19:48:49 -0300

    feat: tela de login, de registro e de recuperar a senha. Navegação básica com expo router

---
+++ src/app/recover.tsx
@@ -0,0 +1,121 @@
+import { useState } from 'react';
+import { View, StyleSheet, Image, Dimensions, Pressable } from 'react-native';
+import { TextInput, Button, Text } from 'react-native-paper';
+import { LinearGradient } from 'expo-linear-gradient';
+import { StatusBar } from 'expo-status-bar';
+import { Link } from 'expo-router';
+
+const screenWidth = Dimensions.get('window').width;
+const aspectRatio = 500 / 250;
+const imageHeight = screenWidth / aspectRatio;
+
+export default function ForgotPasswordScreen() {
+  const [email, setEmail] = useState('');
+
+  return (
+    <View style={{ flex: 1 }}>
+      <StatusBar style="light" />
+
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require('../assets/final.png')}
+            style={styles.image}
+            resizeMode="cover"
+          />
+          <LinearGradient
+            colors={['transparent', '#ffffff']}
+            style={styles.gradient}
+          />
+        </View>
+
+        <View style={styles.card}>
+          <View style={styles.title}>
+            <Text style={{ fontSize: 32, color: "#08364E" }}>Recuperar Senha</Text>
+            <Text style={{ fontSize: 14, color: "#08364E", marginTop: 8 }}>
+              Informe seu e-mail para enviar um link de recuperação
+            </Text>
+          </View>
+
+          <TextInput
+            label="Digite seu e-mail"
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+            autoCapitalize="none"
+            left={<TextInput.Icon icon="email" color="#08364E" />}
+            style={styles.input}
+          />
+
+          <Button mode="contained" style={styles.button}>
+            Enviar Link
+          </Button>
+
+          <View style={{ marginTop: 20 }}>
+            <Link href={"/"} asChild>
+                <Pressable>
+                    <Text style={{ fontSize: 16, color: "#08364E" }}>
+                        Retornar para o Login.
+                    </Text>
+                </Pressable>
+            </Link>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+  },
+  card: {
+    padding: 10,
+    borderRadius: 12,
+    flex: 1,
+    alignItems: 'center',
+  },
+  title: {
+    marginBottom: 20,
+    textAlign: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#08364E",
+    borderRadius: 10,
+    backgroundColor: "#ffffff",
+    marginBottom: 16,
+    width: 300,
+    height: 50,
+  },
+  button: {
+    marginTop: 8,
+    backgroundColor: "#08364E",
+    width: 250,
+    borderRadius: 10,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageContainer: {
+    width: screenWidth,
+    height: imageHeight,
+    marginTop: 0,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  gradient: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 22,
+  },
+});
---
+++ src/app/register.tsx
@@ -0,0 +1,171 @@
+import { useState } from 'react';
+import { View, StyleSheet, Image, Dimensions, Pressable } from 'react-native';
+import { TextInput, Button, Text } from 'react-native-paper';
+import { LinearGradient } from 'expo-linear-gradient';
+import { StatusBar } from 'expo-status-bar';
+import { Link } from 'expo-router'
+
+const screenWidth = Dimensions.get('window').width;
+const aspectRatio = 500 / 250;
+const imageHeight = screenWidth / aspectRatio;
+
+export default function RegisterScreen() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  return (
+    <View style={{ flex: 1 }}>
+      <StatusBar style="light" />
+
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require('../assets/final.png')}
+            style={styles.image}
+            resizeMode="cover"
+          />
+          <LinearGradient
+            colors={['transparent', '#ffffff']}
+            style={styles.gradient}
+          />
+        </View>
+
+        <View style={styles.card}>
+          <View style={styles.title}>
+            <Text style={{ fontSize: 40, color: "#08364E" }}>Cadastrar</Text>
+          </View>
+
+          <TextInput
+            label="Digite seu nome"
+            value={name}
+            onChangeText={setName}
+            left={<TextInput.Icon icon="account" color="#08364E" />}
+            style={styles.input}
+          />
+
+          <TextInput
+            label="Digite seu e-mail"
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+            autoCapitalize="none"
+            left={<TextInput.Icon icon="email" color="#08364E" />}
+            style={styles.input}
+          />
+
+          <TextInput
+            label="Digite sua senha"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+            left={<TextInput.Icon icon="lock" color="#08364E" />}
+            style={styles.input}
+          />
+
+          <TextInput
+            label="Confirme sua senha"
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+            secureTextEntry
+            left={<TextInput.Icon icon="lock-check" color="#08364E" />}
+            style={styles.input}
+          />
+
+          <Button mode="contained" style={styles.button}>
+            Cadastrar
+          </Button>
+
+          <View>
+            <Text style={{ fontSize: 20, color: "#08364E", marginTop: 10, marginBottom: 10 }}>
+              ou
+            </Text>
+          </View>
+
+          <Button
+            mode="contained"
+            icon="google"
+            style={styles.buttonGoogle}
+            labelStyle={{ color: '#08364E', fontWeight: 'bold' }}
+          >
+            Cadastrar com Google
+          </Button>
+
+          <View style={{ marginTop: 20 }}>
+            <Link href={"/"} asChild>
+                <Pressable>
+                    <Text style={{ fontSize: 16, color: "#08364E" }}>
+                    Já possui uma conta? Entrar
+                    </Text>
+                </Pressable>
+            </Link>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+  },
+  card: {
+    padding: 10,
+    borderRadius: 12,
+    flex: 1,
+    alignItems: 'center',
+  },
+  title: {
+    marginBottom: 20,
+    textAlign: 'center',
+    alignSelf: 'flex-start',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#08364E",
+    borderRadius: 10,
+    backgroundColor: "#ffffff",
+    marginBottom: 16,
+    width: 300,
+    height: 50,
+  },
+  button: {
+    marginTop: 8,
+    backgroundColor: "#08364E",
+    width: 250,
+    borderRadius: 10,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonGoogle: {
+    marginTop: 8,
+    backgroundColor: "#ffffff",
+    width: 250,
+    borderRadius: 10,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageContainer: {
+    width: screenWidth,
+    height: imageHeight,
+    marginTop: 0,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  gradient: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 22,
+  },
+});
